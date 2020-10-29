abdd5d4ff30fc5f08dad7a882bb7248a937237c9
Nathan Glick <nateleighglick@hotmail.com>
2020-10-29 07:45:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
    array.join(" and ")
  else 
    oxford_array = array
-   oxford_array[oxford_array.length - 1] = "and oxford_array"
+   oxford_array[oxford_array.length - 1] = "and {oxford_array[oxford_array.length - 1]"
  end
 end
